4334397672d4d0b49e525185b3aab497e14d6dbb
Jerry M <developer.jerrymannel@gmail.com>
2019-12-10 13:45:10 +0530

    removed executables

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 env GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o largeFileGenerator_darvin .
 env GOOS=linux GOARCH=386 go build -ldflags="-s -w" -o largeFileGenerator_x86 .
-env GOOS=windows GOARCH=386 go build -ldflags="-s -w" -o largeFileGenerator_win .
+env GOOS=windows GOARCH=386 go build -ldflags="-s -w" -o largeFileGenerator.exe .
